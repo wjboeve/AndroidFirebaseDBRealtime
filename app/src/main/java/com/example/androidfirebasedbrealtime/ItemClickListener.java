530957a76dd251b9ed148b511935997050484129
Bill Boeve <wjboeve@gmail.com>
2019-06-18 16:51:16 -0400

    First Commit

---
+++ app/src/main/java/com/example/androidfirebasedbrealtime/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.androidfirebasedbrealtime;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view, int position);
+}
---
+++ app/src/main/java/com/example/androidfirebasedbrealtime/Post.java
@@ -0,0 +1,28 @@
+package com.example.androidfirebasedbrealtime;
+
+public class Post {
+    private String title,content;
+
+    public Post(){}
+
+    public Post(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
